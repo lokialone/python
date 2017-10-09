16f0dc4c2efd496fbadb4c92a5432c8bacd1da78
lokialone <648875862@qq.com>
2017-10-10 00:03:03 +0800

    throttle and page depth

--- webScrape/first-crawling/simple_crawlimg.py
+++ webScrape/first-crawling/simple_crawlimg.py
@@ -4,11 +4,20 @@ import urllib2
 import re
 import itertools
 import urlparse
+import robotparser
+import datetime
+import time
 
 #simple
-def download(url,user_agent= 'wswp', num_retries=2):
+def download(url,user_agent= 'wswp', proxy=None, num_retries=2):
     print 'Downloading:', url
     request = urllib2.Request(url, headers = {'User-agent': user_agent})
+    opener = urllib2.build_opener()
+
+    #add proxy
+    if proxy:
+        proxy_pramas = { urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_pramas))
     try:
         html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
@@ -56,25 +65,57 @@ def get_links(html):
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
     return webpage_regex.findall(html)             
 
+# add a delay betweet downloads to the same domain
+class Throttle:
+    def __init__(self, delay):
+        self.delay =  delay
+        self.domains = {}
+
+    def wait (self, url):
+        domain = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+        if (self.delay > 0 and last_accessed is not None):
+            sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.datetime.now()
 
 # link crawling
-def link_crawler(seed_url, link_regex):
+def link_crawler(seed_url, link_regex, max_depth=2):
     crawl_queue = [seed_url]
-    seen = set(crawl_queue)
+    # crawl_queue = Queue.deque([seed_url])
+    seen = {seed_url: 0}
+    # track how many URL's have been downloaded
+    throttle = Throttle(1)
     while crawl_queue:
         url = crawl_queue.pop()
+        throttle.wait(url)
         html = download(url)
-        for link in get_links(html):
-            if re.search(link_regex, link):
-                link = urlparse.urljoin(seed_url, link)
-                print link
-                if link not in seen:
-                    seen.add(link)
-                    crawl_queue.append(link)
+        depth = seen[url]
+        if depth != max_depth:
+            for link in get_links(html):
+                if re.search(link_regex, link):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in seen:
+                        seen[link] = depth + 1
+                        crawl_queue.append(link)
+
+    
+
+# crawling on the basic of robot.txt
+# it's not normal that every web site have robot.txt or sitemap
+def get_robots(url):
+    """Initialize robots parser for this domain
+    """
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp
+
 
 
 if __name__ == '__main__':
     # url = input("Input begin url: ")
     # crawl_sitemap('http://example.webscraping.com/sitemap.xml')
     # crawling_id();
-    link_crawler('http://example.webscraping.com', '/(index|view)')
+    link_crawler('http://example.webscraping.com', '/(index|view)', 1)
