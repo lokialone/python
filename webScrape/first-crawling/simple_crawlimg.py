3f17fdc06d6290b41139779a32db01668adb1cf0
lokialone <648875862@qq.com>
2017-10-09 16:42:43 +0800

    crawling id

--- webScrape/first-crawling/simple_crawlimg.py
+++ webScrape/first-crawling/simple_crawlimg.py
@@ -2,6 +2,7 @@
 
 import urllib2
 import re
+import itertools
 
 #simple
 def download(url,user_agent= 'wswp', num_retries=2):
@@ -26,16 +27,34 @@ def download(url,user_agent= 'wswp', num_retries=2):
 def crawl_sitemap(url):
     #download the sitemap file
     sitemap = download(url)
-    print sitemap
+    # print sitemap
     #extract the sitemap links
     links = re.findall('<loc>(.*?)</loc>', sitemap)
-    print links
+    # print links
     #download each link
     for link in links:
         html = download(link)
         print html
 
 
+# download html according to id
+
+
+def crawling_id():
+    max_errors = 5
+    num_errors = 0
+    for page in itertools.count(1):
+        url = 'http://example.webscraping.com/places/default/view/%d' % page
+        html = download(url)
+        if html is None:
+            num_errors += 1
+            if num_errors == max_errors:
+                break
+        else:
+            num_errors = 0
+    
+
 if __name__ == '__main__':
     # url = input("Input begin url: ")
-    crawl_sitemap('http://example.webscraping.com/sitemap.xml')
+    # crawl_sitemap('http://example.webscraping.com/sitemap.xml')
+    crawling_id();
