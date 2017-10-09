55b84affd7a8b97b8a5b457d01ea0837be91aced
lokialone <648875862@qq.com>
2017-10-09 16:30:21 +0800

    crawling sitemap

--- webScrape/first-crawling/simple_crawlimg.py
+++ webScrape/first-crawling/simple_crawlimg.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python2.7
 
 import urllib2
+import re
 
-def download(url):
+#simple
+def download(url,user_agent= 'wswp', num_retries=2):
     print 'Downloading:', url
+    request = urllib2.Request(url, headers = {'User-agent': user_agent})
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         print 'Downloading error:', e.reason
         html = None
-    print html
+        if num_retries > 0:
+                if hasattr(e, 'code') and 500 <= e.code < 600:
+                    #recursively retry 5XX http errors
+     
+                   download(url, num_retries-1)
+    # test
+    # print html
     return html
 
+
+#download html according to sitemap
+def crawl_sitemap(url):
+    #download the sitemap file
+    sitemap = download(url)
+    print sitemap
+    #extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    print links
+    #download each link
+    for link in links:
+        html = download(link)
+        print html
+
+
 if __name__ == '__main__':
-    url = input("Input begin url: ")
-    download(url)
+    # url = input("Input begin url: ")
+    crawl_sitemap('http://example.webscraping.com/sitemap.xml')
