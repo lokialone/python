527f306045bf49f0080a592df711b00e22ab896f
lokialone <648875862@qq.com>
2017-10-09 17:30:05 +0800

    link crawler

--- webScrape/first-crawling/simple_crawlimg.py
+++ webScrape/first-crawling/simple_crawlimg.py
@@ -3,6 +3,7 @@
 import urllib2
 import re
 import itertools
+import urlparse
 
 #simple
 def download(url,user_agent= 'wswp', num_retries=2):
@@ -16,8 +17,7 @@ def download(url,user_agent= 'wswp', num_retries=2):
         if num_retries > 0:
                 if hasattr(e, 'code') and 500 <= e.code < 600:
                     #recursively retry 5XX http errors
-     
-                   download(url, num_retries-1)
+                    download(url, num_retries-1)
     # test
     # print html
     return html
@@ -38,8 +38,6 @@ def crawl_sitemap(url):
 
 
 # download html according to id
-
-
 def crawling_id():
     max_errors = 5
     num_errors = 0
@@ -52,9 +50,31 @@ def crawling_id():
                 break
         else:
             num_errors = 0
-    
+
+# return a list of links from html
+def get_links(html):
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    return webpage_regex.findall(html)             
+
+
+# link crawling
+def link_crawler(seed_url, link_regex):
+    crawl_queue = [seed_url]
+    seen = set(crawl_queue)
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        for link in get_links(html):
+            if re.search(link_regex, link):
+                link = urlparse.urljoin(seed_url, link)
+                print link
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
+
 
 if __name__ == '__main__':
     # url = input("Input begin url: ")
     # crawl_sitemap('http://example.webscraping.com/sitemap.xml')
-    crawling_id();
+    # crawling_id();
+    link_crawler('http://example.webscraping.com', '/(index|view)')
