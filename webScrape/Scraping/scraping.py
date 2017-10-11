49f7bf392deca98fe87b2ad8746d957c9b7443fb
lokialone <648875862@qq.com>
2017-10-11 16:16:28 +0800

    python relative import

---
+++ webScrape/Scraping/scraping.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('../FirstCrawling')
+from simple_crawling import download
+
+if __name__ == '__main__':
+    download('http://www.baidu.com')
+
+
+
---
+++ webScrape/Scraping/summary.md
@@ -0,0 +1,7 @@
+3抓取网页内容的方式
+1. 正则表达式
+2. beatutiful soup
+```
+    pip2.7 install beautifulsoup4
+```
+3. lxml  (推荐使用)
