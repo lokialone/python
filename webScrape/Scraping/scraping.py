b0fb051a378f87beea158a051453951118c35d4e
lokialone <648875862@qq.com>
2017-10-17 16:33:44 +0800

    scrapy data

--- webScrape/Scraping/scraping.py
+++ webScrape/Scraping/scraping.py
@@ -8,13 +8,30 @@ from ..Common import link_crawler
 
 FIELDS = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
 
+#print res 
 def scrape_callback(url, html):
     if re.search('/view/', url):
         tree = lxml.html.fromstring(html)
         row = [tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content() for field in FIELDS]
         print url, row
+
+# write into cvs 
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv', 'w'))
+        self.fields = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
+        self.writer.writerow(self.fields)
+
+    def __call__(self, url, html):
+        if re.search('/view/', url):
+            tree = lxml.html.fromstring(html)
+            row = []
+            for field in self.fields:
+                row.append(tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content())
+            self.writer.writerow(row)
+
 def run():
-   link_crawler.link_crawler('http://example.webscraping.com/', '/(index|view)', max_depth=1, scrape_callback=scrape_callback)
+   link_crawler.link_crawler('http://example.webscraping.com/', '/(index|view)', max_depth=1, scrape_callback=ScrapeCallback())
 
 if __name__ == '__main__':
     link_crawler.link_crawler('http://example.webscraping.com/', '/(index|view)', max_depth=1, scrape_callback=scrape_callback)
