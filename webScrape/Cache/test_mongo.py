e0d06219edc638dcaa759bc479eb2cccb5b963e4
lokialone <648875862@qq.com>
2017-10-22 12:19:35 +0800

    mongo_test

--- webScrape/Cache/test_mongo.py
+++ webScrape/Cache/test_mongo.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env/python2.7
-# use 
+# need start mongo!!
 
 from pymongo import MongoClient
+import datetime
+import pprint
+
 client = MongoClient('localhost', 27017)
 db = client.test_database
+# post = {"author": "Mike",
+#          "text": "My first blog post!",
+#          "tags": ["mongodb", "python", "pymongo"],
+#          "date": datetime.datetime.utcnow()}
+posts = db.posts
+# post_id = posts.insert_one(post).inserted_id
+
+
 
 if __name__ == "__main__":
-    print db
+    pprint.pprint(posts.find_one())
